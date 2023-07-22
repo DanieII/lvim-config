77c95fb56e2da3b33eea997bf87d2ac7dd41a223
DanieII <danielbogdanov1748@gmail.com>
2023-07-22 16:48:42 +0300

    Added a debugger for python/django

--- config.lua
+++ config.lua
@@ -2,18 +2,73 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
---
 
 lvim.colorscheme = "kanagawa-dragon"
 
 lvim.plugins = {
-  { "rebelot/kanagawa.nvim" },
+  "mfussenegger/nvim-dap-python",
+  "nvim-neotest/neotest",
+  "nvim-neotest/neotest-python",
+  "rebelot/kanagawa.nvim",
 }
+
+-- Testing and Debugging
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function()
+  require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+end)
+
+-- Debug Django
+local dap = require('dap')
+
+dap.configurations.python = {
+  {
+    type = 'python',
+    request = 'launch',
+    name = "Launch file",
+    program = "${file}",
+    pythonPath = "/Library/Frameworks/Python.framework/Versions/3.11/bin/python3", -- Actual Python Path
+  },
+}
+
+table.insert(dap.configurations.python, {
+  type = 'python',
+  request = 'launch',
+  name = 'Django',
+  program = vim.fn.getcwd() .. '/manage.py',
+  args = { 'runserver', '--noreload' },
+})
+
+
+require("neotest").setup({
+  adapters = {
+    require("neotest-python")({
+      dap = {
+        justMyCode = false,
+        console = "integratedTerminal",
+      },
+      args = { "--log-level", "DEBUG", "--quiet" },
+      runner = "pytest",
+    })
+  }
+})
+
+lvim.builtin.which_key.mappings["dm"] = { "<cmd>lua require('neotest').run.run()<cr>",
+  "Test Method" }
+lvim.builtin.which_key.mappings["dM"] = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+  "Test Method DAP" }
+lvim.builtin.which_key.mappings["df"] = {
+  "<cmd>lua require('neotest').run.run({vim.fn.expand('%')})<cr>", "Test Class" }
+lvim.builtin.which_key.mappings["dF"] = {
+  "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Test Class DAP" }
+lvim.builtin.which_key.mappings["dS"] = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" }
 
 lvim.builtin.treesitter.ensure_installed = {
   "python",
 }
 
+-- Formatters and Linters
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { name = "black" },
@@ -26,8 +81,11 @@ require 'lspconfig'.tailwindcss.setup {}
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { { command = "flake8", filetypes = { "python" } } }
 
+
+-- Switching Between Tabs
 lvim.keys.normal_mode["L"] = ":bnext<cr>"
 lvim.keys.normal_mode["H"] = ":bprev<cr>"
+
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "html" })
 local opts = {
