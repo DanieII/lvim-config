aa20e156d962779beef5e8b8a7b8040e33c85511
Daniel Bogdanov <danielbogdanov1748@gmail.com>
2024-02-25 23:54:27 +0200

    Added ruff lsp

--- lua/lsp/languages/python.lua
+++ lua/lsp/languages/python.lua
@@ -1,10 +1,27 @@
+local lspconfig = require "lspconfig"
+local lspmanager = require "lvim.lsp.manager"
 local formatters = require "lvim.lsp.null-ls.formatters"
-local linters = require "lvim.lsp.null-ls.linters"
+
+lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+  return server ~= "ruff_lsp"
+end, lvim.lsp.automatic_configuration.skipped_servers)
+
+lspmanager.setup("pyright", {
+  settings = {
+    pyright = {
+      disableLanguageServices = false,
+      disableOrganizeImports = false
+    },
+    python = {
+      analysis = {
+        ignore = { "*" }
+      }
+    }
+  }
+})
+
+lspconfig.ruff_lsp.setup({ filetypes = { "python" } })
 
 formatters.setup {
-  { name = "black",  filetypes = { "python" } },
   { name = "djlint", filetypes = { "htmldjango" } },
-}
-linters.setup {
-  { exe = "flake8", filetypes = { "python" } },
 }
